787c947e1039ab9144418050b67a254280e674e9
github username <github email address>
2021-05-10 14:49:21 -0700

    got images

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,16 +1,17 @@
 import { Card, Image } from "semantic-ui-react";
 
 const MovieCard = (props) => {
+
+  const img_url = "https://image.tmdb.org/t/p/original"
     return (
 
       <Card raised className="card-size" style={{width: "325px"}}>
         <Card.Content> 
+          <Image src={`${img_url}` + props.movie.poster_path} className="card-image"/>
           <Card.Header>
               {props.movie.title}
           </Card.Header>
-          <Image src={props.movie.poster_path} className="card-image"/>
           <Card.Description>
-              <strong>Overview</strong><br />
               {props.movie.overview}
           </Card.Description> <br />   
           <Card.Meta >
