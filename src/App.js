4b8e6189533ce38037a6def48ee4ba496fd63496
Tri <triphan921@gmail.com>
2021-05-11 13:02:32 -0700

    jwt

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 import MovieContainer from "./Container/MovieContainer";
-import ReviewContainer from "./Container/ReviewContainer"
+// import ReviewContainer from "./Container/ReviewContainer"
 
 import Login from "./Components/Login";
-import Nav from './Components/Nav'
-import Home from './Components/Home'
-import Signup from './Components/Signup'
-import Profile from './Components/Profile'
+import Nav from "./Components/Nav";
+import Home from "./Components/Home";
+import Signup from "./Components/Signup";
+import Profile from "./Components/Profile";
 
 import {
   BrowserRouter as Router,
@@ -22,31 +22,25 @@ class App extends React.Component {
     logged_in: false,
     token: null,
     movies: [],
-    reviews: []
   };
 
   handleLogin = (token) => {
     this.setState({ logged_in: true, token });
   };
 
- 
   getMovies = () => {
     fetch("http://localhost:3000/movies")
       .then((r) => r.json())
       .then((json) => {
         // console.log(data.results)
-        this.setState({ movies: json.data});
+        this.setState({ movies: json.data });
       });
-  };
 
-//  getReviews = () => {
-//     fetch("http://localhost:3000/reviews")
-//     .then(r => r.json())
-//     .then(json => {
-//       console.log(data)
-//       this.setState({reviews: json.data})
-//     })
-//   }
+    const authToken = localStorage.getItem("token");
+    if (authToken) {
+      this.setState({ logged_in: true, token: authToken });
+    }
+  };
 
   componentDidMount = () => {
     this.getMovies();
@@ -56,14 +50,19 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-           {/* <MovieContainer movies={this.state.movies} /> */}
-      {/* //   <ReviewContainer reviews={this.state.reviews} /> */}
+        {/* <MovieContainer movies={this.state.movies} /> */}
+        {/* //   <ReviewContainer reviews={this.state.reviews} /> */}
 
         <Router>
           <Nav logged_in={this.state.logged_in} />
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/movies" component={() => <MovieContainer movies={this.state.movies} />} />
+
+            <Route
+              exact
+              path="/movies"
+              component={() => <MovieContainer movies={this.state.movies} />}
+            />
 
             <Route
               path="/profile"
