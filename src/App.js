f7a95ef4e95f386bf143d5708a89940fed6a175f
Tri <triphan921@gmail.com>
2021-05-10 18:59:35 -0700

    added Home, Login, Nav, Profile and Signup

--- src/App.js
+++ src/App.js
@@ -1,37 +1,91 @@
-import React from 'react';
+import React from "react";
 import "semantic-ui-css/semantic.min.css";
-import './App.css';
+import "./App.css";
 import MovieContainer from "./Container/MovieContainer";
 
-class App extends React.Component {
+import Login from "./Components/Login";
+import Nav from './Components/Nav'
+import Home from './Components/Home'
+import Signup from './Components/Signup'
+import Profile from './Components/Profile'
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+class App extends React.Component {
   state = {
-    movies: []
-  }
+    logged_in: false,
+    token: null,
+    movies: [],
+  };
+
+  handleLogin = (token) => {
+    this.setState({ logged_in: true, token });
+  };
 
   //Backend Requests
-  getMovies = () => {
-    fetch("http://localhost:3000/trendingmovies")
-    .then(r => r.json())
-    .then(data => {
-      // console.log(data.results)
-      this.setState({movies: data.results})
-    })
-  }
+  // getMovies = () => {
+  //   fetch("http://localhost:3000/trendingmovies")
+  //     .then((r) => r.json())
+  //     .then((data) => {
+  //       // console.log(data.results)
+  //       this.setState({ movies: data.results });
+  //     });
+  // };
 
-  componentDidMount = () => {
-    this.getMovies()
-  }
+  // componentDidMount = () => {
+  //   this.getMovies();
+  // };
 
   render() {
     return (
-      <div>
-      <MovieContainer movies={this.state.movies} />
+      <div className="App">
+        {/* <MovieContainer movies={this.state.movies} /> */}
+
+        <Router>
+          <Nav logged_in={this.state.logged_in} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            {/* <Route exact path="/movies" component={Home} /> */}
+
+            <Route
+              path="/profile"
+              component={() => {
+                return this.state.logged_in ? (
+                  <Profile {...this.state} />
+                ) : (
+                  <Redirect to="/login" />
+                );
+              }}
+            />
+
+            <Route
+              path="/login"
+              component={() => <Login handleLogin={this.handleLogin} />}
+            />
+
+            <Route
+              path="/signup"
+              component={() => <Signup handleLogin={this.handleLogin} />}
+            />
+
+            <Route
+              path="/logout"
+              component={() => {
+                localStorage.clear();
+                this.setState({ logged_in: false, token: null });
+                return <Redirect to="/" />;
+              }}
+            />
+          </Switch>
+        </Router>
       </div>
-    )
+    );
   }
 }
-
-
 
 export default App;
---
+++ src/Components/Profile.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Profile extends React.Component {
+
+  componentDidMount(){
+    fetch('http://localhost:3000/pets',{
+      method: 'GET',
+      headers: {
+        'token': this.props.token
+      }
+    }).then(res => res.json())
+    .then(console.log)
+  }
+
+  render(){
+    return (
+      <>
+        Profile Page
+      </>
+    )
+  }
+}
+
+export default Profile;
