72a0267a3be31af348b2707fece3f26d6c456c8d
github username <github email address>
2021-05-10 19:05:29 -0700

    got reviews

--- src/App.js
+++ src/App.js
@@ -2,31 +2,43 @@ import React from 'react';
 import "semantic-ui-css/semantic.min.css";
 import './App.css';
 import MovieContainer from "./Container/MovieContainer";
+import ReviewContainer from "./Container/ReviewContainer"
 
 class App extends React.Component {
 
   state = {
-    movies: []
+    movies: [],
+    reviews: []
   }
 
   //Backend Requests
   getMovies = () => {
-    fetch("http://localhost:3000/trendingmovies")
+    fetch("http://localhost:3000/movies")
     .then(r => r.json())
     .then(data => {
-      // console.log(data.results)
-      this.setState({movies: data.results})
+      this.setState({movies: data})
+    })
+  }
+
+  getReviews = () => {
+    fetch("http://localhost:3000/reviews")
+    .then(r => r.json())
+    .then(data => {
+      console.log(data)
+      this.setState({reviews: data})
     })
   }
 
   componentDidMount = () => {
     this.getMovies()
+    this.getReviews()
   }
 
   render() {
     return (
       <div>
       <MovieContainer movies={this.state.movies} />
+      <ReviewContainer reviews={this.state.reviews} />
       </div>
     )
   }
